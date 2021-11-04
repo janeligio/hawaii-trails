b624ba997ec8903fcb17dcdd866d4f5faa769989
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-03 18:52:58 -1000

    Add simple routing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header>Hello, world.</header>
+      <Navigation />
+      <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+
+  Link
+} from 'react-router-dom';
+
+export default function Navigation() {
+    return (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+          </ul>
+        </nav>
+    );
+}
