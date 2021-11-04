1d02f9a87343bcfb5cfd8af6d9bf48c1296cd8b8
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-03 19:15:57 -1000

    Add sass. Style navigation.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Navigation.sass';
+
 export default function Navigation() {
     return (
-        <nav>
+        <nav className="navigation">
             <ul>
                 <li>
                     <Link to="/">Home</Link>
