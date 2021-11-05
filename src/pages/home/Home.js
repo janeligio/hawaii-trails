7a91e115505ddaf079b673e790acbecfa839d2d7
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-04 19:57:00 -1000

    Implement basic ArcGIS map.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,23 @@
+import { useMap, useWebMap, useEvent } from 'esri-loader-hooks';
+
 export default function Home() {
+    const map = {
+        basemap: 'streets',
+    };
+    const honolulu = [21.306944, -157.858333];
+    const options = {
+        view: {
+            // center: [21, -157],
+            zoom: 4,
+        },
+    };
+    // const [ref] = useMap(map, options);
+
+    const [ref, view] = useWebMap('f9338a96266b48198de612b0b27b580a');
+    useEvent(view, 'click', (e) => console.log(e));
     return (
         <main>
-            <h1>Home</h1>
+            <div style={{ height: '100vh' }} ref={ref} />
         </main>
     );
 }
