8e4108a284e2de278ef9ef3be0cee061325d01a4
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-08 20:12:19 -1000

    Fix styling and removed console log.

--- src/components/LogoutButton/LogoutButton.js
+++ src/components/LogoutButton/LogoutButton.js
@@ -8,9 +8,11 @@ const LogoutButton = () => {
     const { logout } = useAuth0();
     return (
         <button
-            onClick={() => logout({
-                returnTo: window.location.origin,
-            })}
+            onClick={() =>
+                logout({
+                    returnTo: window.location.origin,
+                })
+            }
         >
             Log Out
         </button>
