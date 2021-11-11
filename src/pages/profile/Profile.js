e28cf3d546892c8f6283e4a581c901b3b1fc5de4
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-10 19:37:32 -1000

    Style basic profile page.

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,17 +1,43 @@
 import { useAuth0 } from '@auth0/auth0-react';
-
+import { Paper } from '@mui/material';
+import './Profile.sass';
 export default function Profile() {
     const {
         user: { name, picture, email, nickname },
     } = useAuth0();
 
+    const checkIns = [
+        {
+            id: 1,
+            name: 'Diamond Head',
+            date: '2020-05-01',
+        },
+        {
+            id: 2,
+            name: 'Koko Head',
+            date: '2020-05-01',
+        },
+    ];
     return (
         <main id="profile">
-            <h1>Profile</h1>
-            <img src={picture} alt={name} />
-            <h2>Name: {name}</h2>
-            <p>Email: {email}</p>
-            <p>Nickname: {nickname}</p>
+            <div className="user">
+                <img className="image" src={picture} alt={name} />
+                <div className="info">
+                    <p>{nickname}</p>
+                    <p>{email}</p>
+                </div>
+            </div>
+            <div className="check-in">
+                <h1>Recent check-ins</h1>
+                <div className="check-in-container">
+                    {checkIns.map((checkIn) => (
+                        <Paper key={checkIn.id} className="check-in-item">
+                            <p>{checkIn.date}</p>
+                            <p>{checkIn.name}</p>
+                        </Paper>
+                    ))}
+                </div>
+            </div>
         </main>
     );
 }
